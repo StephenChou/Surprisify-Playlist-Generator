5431b4be589dc965e3bbd3764ccade4eb96f61ea
stephenchou <sccusr@gmail.com>
2020-07-02 18:24:32 -0400

    Changed db size

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ db.app = app
 
 
 class users(db.Model):
-    spotify_id = db.Column(db.String(22), primary_key=True)
-    playlist_id = db.Column(db.String(22), unique=True, nullable=False)
+    spotify_id = db.Column(db.String(50), primary_key=True)
+    playlist_id = db.Column(db.String(25), unique=True, nullable=False)
     first_name = db.Column(db.String(20), unique=False, nullable=True)
 
     def __init__(self, usr_id, pl_id, f_name):
